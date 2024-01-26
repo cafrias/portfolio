3e2fdf2be3f42ead2630e2ce1b7897dbe561c485
Carlos Frias <carlos.a.frias@gmail.com>
2024-01-26 16:40:54 -0300

    chore: setup party town to run GTM on WebWorker

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
