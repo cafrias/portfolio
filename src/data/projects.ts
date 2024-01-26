bc58af4ea018c2cc6d9bced98173e859dd2ef52c
Carlos Frias <carlos.a.frias@gmail.com>
2024-01-26 15:00:59 -0300

    chore: improve styles

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,12 @@
 import type { Project } from "src/models/Project";
 import { Techs } from "../models/Techs";
 
+import portfolioThumbnail from "./assets/portfolio-thumbnail.png";
+
 export const projects: Project[] = [
   {
     title: "Personal Portfolio",
-    thumbnail: "/post_img.webp",
+    thumbnail: portfolioThumbnail,
     description: "My Personal Portfolio where I upload my new projects",
     repoUrl: "https://github.com/cafrias/portfolio",
     techStack: new Set([Techs.ASTRO, Techs.TAILWIND]),
--- src/models/Project.ts
+++ src/models/Project.ts
@@ -2,7 +2,7 @@ import type { Techs } from "./Techs";
 
 export interface Project {
   title: string;
-  thumbnail: string;
+  thumbnail: ImageMetadata;
   description: string;
   repoUrl: string;
   techStack: Set<Techs>;
